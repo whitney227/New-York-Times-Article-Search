d28ac1b22fec52d07c3f15b2137e1a4886c1d3ce
Robert Butler <robwbutler@gmail.com>
2019-03-23 13:00:52 -0400

    first js

---
+++ assets/javascript/nytsearch.js
@@ -0,0 +1,21 @@
+
+//"search" is just a placeholder for whatever the submit button id will be
+
+$("#search").on("click", function(event){
+
+    var search = $("#search-form").val()
+
+    //again "search-form" is just a placeholder for whatever the form ID is
+
+    var entryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "s&api-key=KxrAwEmYuev5f6ywZ8AP9McEYX7yXHac"
+
+    $.ajax({
+        url: entryURL,
+        method: "GET"
+    })
+    .then(function(response) {
+        console.log(response)
+    });
+
+
+})
